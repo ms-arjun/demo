268e7311e73a56c395ba2b4c989984572ba35bb6
ms-arjun <arjunmurugesan11@gmail.com>
2024-01-03 13:51:08 +0530

    arjun

--- demo.js
+++ demo.js
@@ -1,1 +1,9 @@
-alert(123);
+
+
+const fetchWatches = async()=>{
+    let response = await fetch('https://proud-barracuda.cyclic.app/api/watches/all');
+    let finalResponse = await response.json();
+    console.log(finalResponse)
+    return finalResponse;
+}
+
